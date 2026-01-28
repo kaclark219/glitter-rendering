ce1c7cb7d40fc3918014e8b1ceea9bfef3ca0d7c
kaclark219 <kaclark219@gmail.com>
2026-01-27 20:27:25 -0500

    added object color

--- raytracer/src/object.h
+++ raytracer/src/object.h
@@ -1,16 +1,23 @@
 #include "math/point.h"
 #include "math/vec3.h"
+#include "math/color.h"
 
 class Object {
     protected:
         int materialIndex;
+        Color color;
     public:
-        Object() : materialIndex(-1) {}
-        explicit Object(int matIndex) : materialIndex(matIndex) {}
+        Object() : materialIndex(-1), color(Color()) {}
+        Object(int matIndex, const Color &col = Color()) : materialIndex(matIndex), color(col) {}
         virtual ~Object() = default;
 
+        // getters
         int getMaterialIndex() const { return materialIndex; }
+        const Color& getColor() const { return color; }
+
+        // setters
         void setMaterialIndex(int matIndex) { materialIndex = matIndex; }
+        void setColor(const Color &col) { color = col; }
 
         // intersect ray (origin as Point, direction as Vec3), returns hit distance in t
         virtual bool intersect(const Point& rayOrigin, const Vec3& rayDirection, float& t) const = 0;
